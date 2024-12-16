1e8563bf9ec4e543cef291dbbe63b8143b8f5ce0
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2024-12-16 17:16:02 +0600

    slider add

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
                   
             <div className='w-full bg-white py-[20px]'>
               
-                    <div className='w-full text-center text-[12px] text-black tracking-[3px]'>© {new Date()?.getFullYear()} নবযুগ ক্রীড়াচক্র  | Developed by <a href={"https://www.appifydevs.com/"} target='_blank' rel="noreferrer"><span className='text-[#f24535] ml-[2px]'> Appify</span><span className='text-[#131440]'>Devs</span><span className='text-[#f24535]'>. </span></a></div>
+                    <div className='w-full text-center text-[12px] text-black tracking-[3px]'>© {new Date()?.getFullYear()} নবযুগ ক্রীড়াচক্র  | Developed by <a href={"https://imtiazikbal.vercel.app/"} target='_blank' rel="noreferrer"><span className='text-[#f24535] ml-[2px]'> Imtiaz Ikbal </span></a></div>
               
             </div>
        
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,13 +1,57 @@
+import React, { useState } from 'react';
+
 const HeroSection = () => {
+    // Set up state for the current image index
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    // Array of image URLs for the slider
+    const images = [
+        "/team1.png", // Example image 1
+        "/team2.png", // Example image 2
+        "/team3.png", // Example image 3
+        "/team4.png", // Example image 3
+
+    ];
+
+    // Function to go to the next image
+    const nextImage = () => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    // Function to go to the previous image
+    const prevImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
     return (
         <div className="flex flex-col lg:flex-row justify-between gap-2 py-2">
-            <div><img src="https://placehold.co/1100x810" alt="" /></div>
+            <div className="relative">
+                <img
+                    src={images[currentImageIndex]}
+                    alt="Slider Image"
+                    className="w-full h-auto hover:scale-105 transition duration-500 ease-in-out"
+                />
+                <button
+                    onClick={prevImage}
+                    className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-black text-white p-2"
+                >
+                    &#8249;
+                </button>
+                <button
+                    onClick={nextImage}
+                    className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-black text-white p-2"
+                >
+                    &#8250;
+                </button>
+            </div>
             <div className="flex flex-col gap-2">
                 <div>
-                    <img src="https://placehold.co/400" alt="" />
+                    <img className='hover:scale-105 transition duration-500 ease-in-out' src="https://placehold.co/400" alt="Thumbnail 1" />
                 </div>
                 <div>
-                    <img src="https://placehold.co/400" alt="" />
+                    <img className='hover:scale-105 transition duration-500 ease-in-out' src="https://placehold.co/400" alt="Thumbnail 2" />
                 </div>
             </div>
         </div>
