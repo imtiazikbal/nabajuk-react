866c218e55dab632e179ddd44e4225d64dfeb2a4
imtiazikbal <imtiaz.ikbal.com@gmail.com>
2024-12-17 21:40:06 +0600

    useEffect problem solve

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import SocialIcon from './sections/SocialIcon';
 import Header from './sections/Header';
 import HeroSection from './sections/HeroSection';
-import Scroll from './sections/Scroll';
 import Footer from './sections/Footer';
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
     <div className="App h-screen w-[80%] m-auto py-4" >
   <SocialIcon />
   <Header />
-  {/* <Scroll /> */}
   <HeroSection />
   <Footer />
     </div>
